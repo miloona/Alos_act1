44aceb06720c50ebc193e76d3171a9bde11d82fc
miloona <101870320+miloona@users.noreply.github.com>
2022-03-19 11:58:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var unirest = require('unirest')
 
-function name_starts_with(podcasts, letter) {
-    return podcasts.filter(podcast => podcast.name[0] == letter)
+function name_starts_with(client, letter) {
+    return client.filter(client => client.name[0] == letter)
 }
 
-var req = unirest('GET', 'http://localhost:3000/podcasts').headers({
+var req = unirest('GET', 'http://localhost:3000/client').headers({
     "cache-control": "no-cache" 
 }).then((res) => {
     if (res.error) throw new Error(res.error)
-    var first_10_podcasts = res.body.slice(0, 10)
-    console.log("FIRST 10 PODCASTS:\n")
+    var first_10_client = res.body.slice(0, 10)
+    console.log("FIRST 10 CLIENT:\n")
     console.log(first_10_podcasts)
     console.log("\n\n\n\n")
-    console.log("PODCASTS THAT START WITH THE LETTER M:\n")
+    console.log("CLIENT THAT START WITH THE LETTER M:\n")
     console.log(name_starts_with(res.body, "M"))
