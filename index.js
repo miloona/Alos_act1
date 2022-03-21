4a6c2b2b02afefaa2d871a00a1ff496a8dfe8f9f
miloona <101870320+miloona@users.noreply.github.com>
2022-03-21 21:45:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var unirest = require('unirest')
 
-function name_starts_with(client, letter) {
-    return client.filter(client => client.name[0] == letter)
+function name_starts_with(trip, letter) {
+    return client.filter(trip => trip.name[0] == letter)
 }
 
-var req = unirest('GET', 'http://localhost:3000/client').headers({
+var req = unirest('GET', 'http://localhost:3000/trip').headers({
     "cache-control": "no-cache" //Q3:"response" peut ètre stocké dans le cache si validé par le serveur d'origine, mème si "response" est non-cache
 }).then((res) => {
     if (res.error) throw new Error(res.error)
-    var first_10_client = res.body.slice(0, 10)
+    var first_10_trip = res.body.slice(0, 10)
     console.log("FIRST 10 CLIENT:\n")
     console.log(first_10_podcasts)
     console.log("\n\n\n\n")
-    console.log("CLIENT THAT START WITH THE LETTER M:\n")
+    console.log("TRIP THAT START WITH THE LETTER M:\n")
     console.log(name_starts_with(res.body, "M"))
