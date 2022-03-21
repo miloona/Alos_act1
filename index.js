8348e54d8a5e04bf48830a9275f0bdfc53f4da14
miloona <101870320+miloona@users.noreply.github.com>
2022-03-21 22:08:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-var unirest = require('unirest')
 
-function name_starts_with(trip, letter) {
-    return client.filter(trip => trip.name[0] == letter)
+var listing = { 
+  'repeat(100)':{
+   id: JG.objectId(),
+  email() {
+    return (
+      _.snakeCase(this.profile.name) +
+      '@' +
+      'heli.com' 
+      
+    ).toLowerCase();
+  },
+  username() {
+    return (_.words(this.profile.name)[0]+moment(this.profile.age).format('YY')).toLowerCase();
+
+  },
+    profile: 
+  {
+  index: JG.index(),
+  name:     `${JG.firstName()} ${JG.lastName()}`,
+  age: `${JG.integer(20, 60)}`,
+  country: JG.country(),
+  
+    picture() {
+    return (
+           'http://placehold.it/32x32/picture/'+
+      _.snakeCase(this.name) +'/'+
+       _.snakeCase(this.index)
+    ).toLowerCase();
+  },
+   email() {
+    return (
+      _.snakeCase(this.name) +
+      '@' +
+      'heli.com' 
+    ).toLowerCase();},
+    phone: `${numeral(JG.floating(30,120,6)).format('+00.00,00.00')}`,
+  name_Helipaddy: `${JG.firstName()} `,
+  expertise:JG.random('Beginner', 'professional'),
+  Trip_price:`${numeral(JG.floating(30,120,2)).format('$0,0.00')}`,
+  number_of_trips:parseInt(JG.floating(0,50, 2)),
+   },
+  
+  
+  trip:
+  {
+  time_to_go:JG.date(new Date(2020, 0, 1), new Date(2023, 0, 1)),
+  go_to:JG.city(),
+  flying_hours: parseInt(JG.floating(0,10, 2)),
+  number_of_passengers:parseInt(JG.floating(0,7, 2)),
+  Quality:JG.random('Good', 'Normal','Bad'),
+  },
+
+});
+]
 }
 
-var req = unirest('GET', 'http://localhost:3000/trip').headers({
-    "cache-control": "no-cache" //Q3:"response" peut ètre stocké dans le cache si validé par le serveur d'origine, mème si "response" est non-cacheable
-}).then((res) => {
-    if (res.error) throw new Error(res.error)
-    var first_10_trip = res.body.slice(0, 10)
-    console.log("FIRST 10 CLIENT:\n")
-    console.log(first_10_podcasts)
-    console.log("\n\n\n\n")
-    console.log("TRIP THAT START WITH THE LETTER M:\n")
-    console.log(name_starts_with(res.body, "M"))
