9b1d3efbbf11463a93d6ac3a95b21ca23c0b3283
miloona <101870320+miloona@users.noreply.github.com>
2022-03-21 16:49:55 +0100

    Update listing.js

--- listing.js
+++ listing.js
@@ -1,24 +1,57 @@
 var listing_1 = { 
   'repeat(100)':{
-  'profile': [{
-  id: {{objectId()}},
-  index: {{index()}},
-  Helicopter_captain_name:{{firstName()}{lastName()}}`,
-  Date_of_birth:{{date()}},
-  country: {{country()}},
-  name_Helipaddy:{{firstName()}} `,
-  expertise:{{random('Beginner', 'professional')}},
-  Trip_price:{{numeral(floating(30,120,2)).format('$0,0.00')}}`,
-  number_of_trips:{{parseInt(floating(0,50, 2))}},
-   },
- trip:{
+  JG.repeat(100,{
   
-  time_to_go:{{date()}},
-  go_to:{{city()}},
-  flying_hours: {{parseInt(floating(0,10, 2))}},
-  number_of_passengers:{{parseInt(floating(0,7, 2))}},
-  Quality:{{random('Good', 'Normal','Bad')}},
+   id: JG.objectId(),
+  email() {
+    return (
+      _.snakeCase(this.profile.name) +
+      '@' +
+      'heli.com' 
+      
+    ).toLowerCase();
   },
+  username() {
+    return (_.words(this.profile.name)[0]+moment(this.profile.age).format('YY')).toLowerCase();
+
+  },
+    profile: 
+  {
+  index: JG.index(),
+  name:     `${JG.firstName()} ${JG.lastName()}`,
+  age: `${JG.integer(20, 60)}`,
+  country: JG.country(),
   
+    picture() {
+    return (
+           'http://placehold.it/32x32/picture/'+
+      _.snakeCase(this.name) +'/'+
+       _.snakeCase(this.index)
+    ).toLowerCase();
+  },
+   email() {
+    return (
+      _.snakeCase(this.name) +
+      '@' +
+      'heli.com' 
+    ).toLowerCase();},
+    phone: `${numeral(JG.floating(30,120,6)).format('+00.00,00.00')}`,
+  name_Helipaddy: `${JG.firstName()} `,
+  expertise:JG.random('Beginner', 'professional'),
+  Trip_price:`${numeral(JG.floating(30,120,2)).format('$0,0.00')}`,
+  number_of_trips:parseInt(JG.floating(0,50, 2)),
+   },
+  
+  
+  trip:
+  {
+  time_to_go:JG.date(new Date(2020, 0, 1), new Date(2023, 0, 1)),
+  go_to:JG.city(),
+  flying_hours: parseInt(JG.floating(0,10, 2)),
+  number_of_passengers:parseInt(JG.floating(0,7, 2)),
+  Quality:JG.random('Good', 'Normal','Bad'),
+  },
+
+});
 ]
 }
